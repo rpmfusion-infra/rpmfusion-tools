a771408bdf31e4e56b902f9178ecc6e8706b744f
Leigh Scott <leigh123linux@gmail.com>
2019-11-19 23:38:03 +0000

    F29 is EOL

--- sigulsign_unsigned.py
+++ sigulsign_unsigned.py
@@ -36,13 +36,9 @@ CLIENTCERT = os.path.expanduser('~/.rpmfusion.cert')
 
 KEYS = {
     'rpmfusion-cuda-2019': {'id': 'c8d47bb7', 'v3': True},
-    'rpmfusion-fedora-free-28': {'id': '09eab3f2', 'v3': True},
-    'rpmfusion-fedora-free-29': {'id': '42f19ed0', 'v3': True},
     'rpmfusion-fedora-free-30': {'id': 'c0aeda6e', 'v3': True},
     'rpmfusion-fedora-free-31': {'id': 'c481937a', 'v3': True},
     'rpmfusion-fedora-free-32': {'id': '100bcd92', 'v3': True},
-    'rpmfusion-fedora-nonfree-28': {'id': '7f858107', 'v3': True},
-    'rpmfusion-fedora-nonfree-29': {'id': 'd6841af8', 'v3': True},
     'rpmfusion-fedora-nonfree-30': {'id': '1d14a795', 'v3': True},
     'rpmfusion-fedora-nonfree-31': {'id': '54a86092', 'v3': True},
     'rpmfusion-fedora-nonfree-32': {'id': '6dc1be18', 'v3': True},
