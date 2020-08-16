1180d84a3cbd9626566629050e7d94c219e6b1ff
Leigh Scott <leigh123linux@gmail.com>
2020-08-16 18:53:47 +0100

    Update mass-rebuild build directory

--- mass-rebuild.py
+++ mass-rebuild.py
@@ -24,7 +24,7 @@ targets = ['f33-%s-candidate' % flavor , 'rawhide-%s' % flavor, 'f33-%s' % flavo
 epoch = '2020-08-08 06:00:00.000000' # rebuild anything not built after this date
 user = 'RPM Fusion Release Engineering <leigh123linux@gmail.com>'
 comment = '- Rebuilt for https://fedoraproject.org/wiki/Fedora_33_Mass_Rebuild'
-workdir = os.path.expanduser('~/rpmfusion/new/nonfree/massrebuild')
+workdir = os.path.expanduser('~/rpmfusion/new/free/massrebuild')
 enviro = os.environ
 target = 'f33-%s' % flavor
 
